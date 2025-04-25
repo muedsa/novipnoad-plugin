ccdf6f893710f9257a69ae80c794548eda87e1c8
MUEDSA <7676275+muedsa@users.noreply.github.com>
2025-04-25 10:17:46 +0800

    release: 0.0.12

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.muedsa.tvbox.novipnoad"
         minSdk = 24
         targetSdk = 35
-        versionCode = 11
-        versionName = "0.0.11"
+        versionCode = 12
+        versionName = "0.0.12"
     }
 
     signingConfigs {
