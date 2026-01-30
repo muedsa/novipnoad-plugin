f9e2d5b6ddb1e26e2585606a948d3bf4b24f7805
MUEDSA <7676275+muedsa@users.noreply.github.com>
2026-01-30 17:18:09 +0800

    chore(api): 更新到 1b643c3

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,4 +9,5 @@ plugins {
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.serialization) apply false
+    alias(libs.plugins.kotlin.binary.compatibility.validator) apply  false
 }
